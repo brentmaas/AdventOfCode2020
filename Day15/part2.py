b0da007c5fd24bfcb000244087f4924d8315d4d9
Brent Maas <brent.maas@hotmail.com>
2020-12-15 12:21:47 +0100

    Day 15

---
+++ Day15/part2.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+nums = np.loadtxt("Input.txt", delimiter=",", dtype=np.int32)
+
+occ = {}
+last = nums[-1]
+for i in range(len(nums)-1):
+    occ[nums[i]] = i
+
+for i in range(len(nums)-1, 29999999):
+    if last in occ:
+        newlast = i - occ[last]
+        occ[last] = i
+        last = newlast
+    else:
+        occ[last] = i
+        last = 0
+
+print(last)
